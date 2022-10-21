79d5beac8c1f8ce7b4cde0215ec93d275caa8e5d
DaniilKun <daniilkun1997@mail.ru>
2022-10-21 19:58:37 +0400

    com

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1WZfKG76HXAyO8DJmGOJo6bj_bSsy7VPoVQ&usqp=CAU" />
+    </header>
+  );
+};
+
+export default Header
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div className="item">
+        <a>Profile</a>
+      </div>
+      <div className="item">
+        <a>Messages</a>
+      </div>
+      <div className="item">
+        <a>News</a>
+      </div>
+      <div className="item">
+        <a>Music</a>
+      </div>
+      <div className="item">
+        <a>Settings</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
