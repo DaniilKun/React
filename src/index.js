8919fa3617ff541eddbf8310be39714fff202e84
DaniilKun <daniilkun1997@mail.ru>
2022-11-07 08:02:20 +0400

    com

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+import state, {subscribe} from "./redux/state";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import state from './redux/state';
-import {rerenderEntireTree} from './render'
+import { addPost, updateNewPostText } from "./redux/state";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+let rerenderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        addPost={addPost}
+        updateNewPostText={updateNewPostText}
+      />
+    </React.StrictMode>
+  );
+};
 
-
-rerenderEntireTree(state)
-
-
+rerenderEntireTree(state);
+subscribe(rerenderEntireTree)
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from '../render'
+let rerenderEntireTree = () => {
+  console.log('brabus')
+}
 
 let state = {
     profilePage:{
@@ -29,7 +31,7 @@ let state = {
  
 };
 
-export let addPost = () => {
+export const addPost = () => {
 
 let newPost = {
   id:5,
@@ -43,10 +45,15 @@ let newPost = {
 }
 
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
 
   state.profilePage.newPostText = newText
   rerenderEntireTree(state)
+}
+
+export const subscribe = (observer) => {
+
+  rerenderEntireTree = observer
 }
 
 export default state
