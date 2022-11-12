f993014f24654f22f11038931bfb65f50287d7bd
DaniilKun <daniilkun1997@mail.ru>
2022-11-12 23:09:13 +0400

    com

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+
 
 
 const MyPosts = (props) => {
@@ -12,12 +14,13 @@ const MyPosts = (props) => {
   let newPostElement = React.createRef()
 
   let addPost = () => {
-    props.dispatch({type: 'ADD-POST'})
+    props.dispatch(addPostActionCreator())
   }
 
   let onPostChange = () => {
 let text = newPostElement.current.value
-props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText:text})
+let action = updateNewPostTextActionCreator(text)
+props.dispatch(action)
   }
 
   return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,8 @@
+const ADD_POST = 'ADD-POST'
+
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
+
+
 let store = {
   _state: {
     profilePage: {
@@ -38,7 +43,7 @@ let store = {
 
   
   dispatch(action) {
-    if (action.type ==='ADD-POST') {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 5,
         message: this._state.profilePage.newPostText,
@@ -48,12 +53,17 @@ let store = {
       this._state.profilePage.posts.push(newPost);
       this._state.profilePage.newPostText = "";
       this._callSubscriber(this._state);
-  } else if (action.type ==='UPDATE-NEW-POST-TEXT') {
+  } else if (action.type === UPDATE_NEW_POST_TEXT) {
     this._state.profilePage.newPostText = action.newText;
     this._callSubscriber(this._state);
   }
   }
 };
+
+export const addPostActionCreator = () => ({type:ADD_POST})
+
+export const updateNewPostTextActionCreator = (text) => 
+({type: UPDATE_NEW_POST_TEXT, newText:text,})
 
 export default store;
 window.state = store;
