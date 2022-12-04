ce9e07eee43283e547c140ebec911fddccfc47ab
DaniilKun <daniilkun1997@mail.ru>
2022-12-04 08:15:44 +0400

    com

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -9,10 +9,10 @@ const Dialogs = (props) => {
   let state = props.dialogsPage
 
   let dialogsElement = state.dialogs.map((d) => (
-    <DialogItem name={d.name} id={d.id} />
+    <DialogItem name={d.name} key={d.id} id={d.id} />
   ));
   let messagesElements = state.messages.map((m) => (
-    <Message message={m.message} />
+    <Message message={m.message} key={m.id}/>
   ));
   let newMessageBody = state.newMessageBody
 
