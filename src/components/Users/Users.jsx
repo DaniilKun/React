3b5f2fa8d2af9e9476469d6652c6a375c41c7d54
DaniilKun <daniilkun1997@mail.ru>
2022-12-15 19:56:32 +0400

    com

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,35 +1,16 @@
 import React from "react";
 import styles from "./users.module.css";
+import  axios from "axios";
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-      props.setUsers([
-    {
-      id: 1,
-      photoUrl: "https://тайна-вашего-имени.рф/img/imena/dmitriy.jpg",
-      followed: false,
-      fullName: "Daniil",
-      status: "i am a boss1",
-      location: { city: "Samara", country: "Russia" },
-    },
-    {
-      id: 2,
-      photoUrl: "https://тайна-вашего-имени.рф/img/imena/dmitriy.jpg",
-      followed: true,
-      fullName: "Sasha",
-      status: "i am a boss2",
-      location: { city: "Kinel", country: "Russia" },
-    },
-    {
-      id: 3,
-      photoUrl: "https://тайна-вашего-имени.рф/img/imena/dmitriy.jpg",
-      followed: false,
-      fullName: "Andrey",
-      status: "i am a boss3",
-      location: { city: "Moscow", country: "Russia" },
-    },
-  ]);}
+
+      axios.get('https://social-network.samuraijs.com/api/1.0/users') .then(response => {
+        props.setUsers(response.data.items)
+      })
+
+      }
 
   return (
     <div>
@@ -61,12 +42,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{'u.location.country'}</div>
+              <div>{'u.location.city'}</div>
             </span>
           </span>
         </div>
